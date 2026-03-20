1ab10cfc37a21bfccac20072d0b938b8ac874452
iKzuu <anangsetiaji30@gmail.com>
2026-03-20 21:26:47 +0700

    ResumeCard update

--- src/components/ui/ResumeCard.jsx
+++ src/components/ui/ResumeCard.jsx
@@ -1,18 +1,27 @@
-import { jetbrains, pixel, poppins } from "@/lib/font"
+import { jetbrains, poppins } from "@/lib/font";
+import React from "react";
 
-const ResumeCard = ({company, role, date, description}) => {
+
+const ResumeCard = ({company, role, date, description, arrangement, type, location}) => {
+
+    const tags = [arrangement, type];
+    const info = [company, location];
+
     return (
-        <div className="flex flex-col md:flex-row gap-4 p-4 rounded-sm justify-center md:items-start lg:items-center backdrop-blur-sm shadow-accent-soft">
-            <h1 className={`${pixel.className} text-lg lg:text-2xl font-semibold`}>{company}</h1>
-            <div className="bg-neutral w-full h-[2px] md:w-[2px] md:h-auto md:self-stretch"></div>
-            <div className="flex flex-col gap-2">
-                <div className="flex flex-row justify-between items-center">
-                    <h1 className={`${jetbrains.className} text-sm lg:text-lg font-semibold`}>{role}</h1>
-                    <h4 className={`${jetbrains.className} text-xs lg:text-sm font-medium`}>{date}</h4>
+        <div className="flex flex-col gap-2">
+            <div className="flex flex-col md:flex-row justify-between md:items-center">
+                <h1 className={`${poppins.className} text-xl lg:text-2xl font-semibold text-accent`}>{role}</h1>
+                <div className="flex flex-row items-center gap-2">
+                    {tags.map((tag, index) => (
+                        <span key={index} className={`${jetbrains.className} text-xs px-2 py-1 text-black ${tag ? "bg-light" : ""}`}>
+                            {tag}
+                        </span>
+                    ))}
                 </div>
-
-                <p className={`${jetbrains.className} text-xs lg:text-base text-justify font-normal`}>{description}</p>
             </div>
+            <h4 className={`${jetbrains.className} text-sm text-semilight lg:text-lg font-semibold break-words`}>{info.join(" • ")}</h4>
+            <h4 className={`${jetbrains.className} text-xs lg:text-lg font-semibold`}>{date}</h4>
+            <p className={`${poppins.className} text-xs lg:text-base text-justify font-normal`}>{description}</p>
         </div>
     );
 }
