1cc9b468f9521689d0f17d6d1da6a804f20ca2ef
iKzuu <anangsetiaji30@gmail.com>
2026-03-20 21:28:08 +0700

    add a divider line

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { jetbrains} from "../../lib/font";
 import ResumeCard from "../ui/ResumeCard";
 import RetroHeading from "../ui/RetroHeading";
@@ -10,27 +11,35 @@ const AboutSection = () => {
                 <div className="z-10 flex flex-col gap-4">
                     <RetroHeading text="About Me"/>
 
-                    <p className={`${jetbrains.className} text-sm lg:text-lg text-justify p-6 rounded-sm bg-dark/60 backdrop-blur-xs shadow-accent-soft lg:max-w-[80%] 2xl:max-w-1/2`}>I’m passionate about building interfaces that are simple, modern, and easy to use. I believe good design isn’t about complexity, it’s about clarity and purpose. I enjoy turning ideas into clean, responsive layouts that users love.</p>
+                    <p className={`${jetbrains.className} text-sm lg:text-lg text-justify p-6 rounded-sm bg-dark/60 backdrop-blur-xs shadow-accent-soft lg:max-w-[80%] 2xl:max-w-1/2`}>Frontend Developer with experience in JavaScript, Next.js, and Flutter, focused on building responsive, modern, and user-friendly web and mobile applications.</p>
                 </div>
 
                 <div className="z-10 flex flex-col items-end gap-4">
                     <RetroHeading text="Work Experience"/>
-                    <div className="flex flex-col gap-4 sm:w-full lg:max-w-[80%] 2xl:max-w-1/2">
-                        {data.workExperience.map((exp) => (
-                            <ResumeCard 
-                                key={exp.id}
-                                company={exp.company}
-                                role={exp.role}
-                                date={exp.date}
-                                description={exp.desc}
-                            />
+                    <div className="flex flex-col p-2 md:p-4 gap-4 w-full lg:max-w-[80%] 2xl:max-w-1/2 backdrop-blur-sm shadow-accent-soft">
+                        {data.workExperience.map((exp, index) => (
+                            <React.Fragment key={exp.id}>
+                                <ResumeCard 
+                                    company={exp.company}
+                                    role={exp.role}
+                                    date={exp.date}
+                                    description={exp.desc}
+                                    arrangement={exp.arrangement}
+                                    type={exp.type}
+                                    location={exp.location}
+                                />
+
+                                {index !== data.workExperience.length - 1 && (
+                                    <div className="border-t-2 border-neutral"></div>
+                                )}
+                            </React.Fragment>
                         ))}
                     </div>
                 </div>
 
                 <div className="z-10 flex flex-col gap-4">
                     <RetroHeading text="Education"/>
-                    <div className="flex flex-col gap-4 sm:w-full lg:max-w-[80%] 2xl:max-w-1/2">
+                    <div className="flex flex-col p-2 md:p-4 gap-4 w-full lg:max-w-[80%] 2xl:max-w-1/2 backdrop-blur-sm shadow-accent-soft">
                         {data.education.map((edu) => (
                             <ResumeCard 
                                 key={edu.id}
@@ -38,6 +47,7 @@ const AboutSection = () => {
                                 role={edu.role}
                                 date={edu.date}
                                 description={edu.desc}
+                                location={edu.location}
                             />
                         ))}
                     </div>
