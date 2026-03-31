b03dfed1a17fbeed841126041234bb9ec9e510c2
iKzuu <anangsetiaji30@gmail.com>
2026-03-31 21:11:12 +0700

    little change

--- src/components/ui/Chip.jsx
+++ src/components/ui/Chip.jsx
@@ -1,6 +1,6 @@
 const Chip = ({text}) => {
     return (
-        <div className="p-2 md:p-4 rounded-[4px] text-xs font-semibold bg-dark/60 backdrop-blur-sm shadow-accent-soft">
+        <div className="p-2 md:p-4 rounded-sm text-xs font-semibold bg-dark/60 backdrop-blur-sm shadow-accent-soft">
             <h3 className="text-md md:text-xl">{text}</h3>
         </div>
     );
