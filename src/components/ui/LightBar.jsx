9766ae714f6312b73ee478f8adb818f81c186175
iKzuu <anangsetiaji30@gmail.com>
2026-04-01 17:10:59 +0700

    some change

--- src/components/ui/LightBar.jsx
+++ src/components/ui/LightBar.jsx
@@ -1,24 +1,24 @@
 const Lightbar = () => {
     return (
-        <div className="z-30 absolute top-0 left-1/2 -translate-x-1/2 w-[260px] pointer-events-none">
+        <div className="z-30 absolute top-0 left-1/2 -translate-x-1/2 w-65 pointer-events-none">
             <div className="
                 absolute top-0 left-1/2 -translate-x-1/2
-                w-[120%] h-[300px]
+                w-[120%] h-75
                 blur-[120px]
                 rounded-full
             "></div>
 
             <div className="
                 absolute inset-0
-                h-[180px]
-                bg-gradient-to-b from-accent/40 to-transparent
+                h-45
+                bg-linear-to-b from-accent/40 to-transparent
                 blur-[80px]
                 opacity-60
             "></div>
 
             <div className="
                 absolute inset-0
-                h-[40px]
+                h-10
                 bg-accent
                 blur-3xl
                 opacity-70
@@ -26,7 +26,7 @@ const Lightbar = () => {
 
             <div className="
                 relative
-                h-[8px]
+                h-2
                 bg-accent
                 shadow-[2px_4px_60px_#74AEFF]
             "></div>
