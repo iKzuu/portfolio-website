509f320aa014f4b034aefe58d0cb6392a8f496fb
iKzuu <anangsetiaji30@gmail.com>
2026-05-11 16:03:40 +0700

    add projects data for Zuunime and Sneakcommerce

---
+++ src/data/projects.js
@@ -0,0 +1,19 @@
+export const projects = [
+    {
+        id: 1,
+        image: "/images/web-icon.svg",
+        name: "Zuunime",
+        description: "Zuunime is an Android app for viewing information about anime or manga that uses the API from Jikan API.",
+        href: "https://github.com/iKzuu",
+        tech: ["Flutter", "Dart", "REST API"],
+    },
+    
+    {
+        id: 2,
+        image: "/images/web-icon.svg",
+        name: "Sneakcommerce",
+        description: "E-commerce sneaker app with cart, wishlist, and checkout features.",
+        href: "https://github.com/iKzuu",
+        tech: ["Flutter", "Hive", "Provider"],
+    },
+]
